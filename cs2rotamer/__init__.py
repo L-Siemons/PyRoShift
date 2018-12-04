1bc36bf9a510932db90c3a0bf3c0db305e3fa809
L_siemons <lucas.siemons@gmail.com>
2018-12-04 16:43:26 +0000

    4.12.18 now also imports ReadIn_measured_C_shifts()

--- cs2rotamer/__init__.py
+++ cs2rotamer/__init__.py
@@ -1,5 +1,5 @@
+from test import *
 
 from GeneticAlgorithm import *
 from parameterClasses import *
-from fiIeIO import setup_Fitted_DFT
-from test import *
+from fiIeIO import setup_Fitted_DFT, ReadIn_measured_C_shifts
