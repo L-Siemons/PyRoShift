0150d4f6a6026808e1498e14f77617e88221ff85
L_siemons <lucas.siemons@gmail.com>
2018-12-04 16:44:06 +0000

    4.12.18 modified to fit new algorithm configuration

--- exmapleRun/RunCs2Rotamer.py
+++ exmapleRun/RunCs2Rotamer.py
@@ -7,13 +7,13 @@ import time
 
 
 if __name__ == '__main__':
-    
+
     #====================================================================
     # Arguments
     #====================================================================
 
     parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter, epilog=" ")
-    parser.add_argument("-f", type=str, default='InputFile.txt',help="configuration file containing the algorithm configuration")    
+    parser.add_argument("-f", type=str, default='InputFile.txt',help="configuration file containing the algorithm configuration")
     args = parser.parse_args()
     config_file = args.f
 
@@ -24,12 +24,13 @@ if __name__ == '__main__':
     params = c.ReadInputFile(config_file)
     calRes = params.calcRes
     expshifts = params.expShifts
-    average_CS, experimental_CS = c.setup_Fitted_DFT(calRes,  expshifts,'R', params)
-    
+    experimental_CS, SSE = c.ReadIn_measured_C_shifts(params.expShifts)
+    average_CS = c.setup_Fitted_DFT(calRes,SSE, params)
+
     start = time.time()
     c.algo(average_CS, experimental_CS, params)
     end = time.time()
     print 'time taken: ', end - start
 
 
-    
+
