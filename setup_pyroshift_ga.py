c89597581af95e9e541215a5beab146adc66dfee
Lucas Siemons <lucasSiemons@mac-6c4008b6e69e.home>
2018-12-22 16:03:58 +0000

    22.12.18 added a setup.y for pyroshiftGA

---
+++ setup_pyroshift_ga.py
@@ -0,0 +1,36 @@
+from distutils.core import setup
+from setuptools import setup, find_packages
+from Cython.Build import cythonize
+
+descrip='''
+
+     ====// cs2rotamer \\\\====
+
+This package looks to determine protien
+side-chain rotamer populations from chemical
+shifts.
+
+Currently only isoleucine is implemented here.
+This package is principally maintained by
+Lucas Siemons
+
+Author:
+Lucas Siemons
+
+'''
+
+setup(
+    name='pyroshiftGA',
+    version='1.0',
+    author='L. Siemons',
+    author_email='zcbtla0@ucl.ac.uk',
+    packages=find_packages(),
+    #scripts=[''],
+    #url='http://pypi.python.org/pypi/TowelStuff/',
+    license='LICENSE.txt',
+    description=descrip,
+    long_description=open('README.md').read(),
+    #install_requires=['os','sys','random','numpy','time',],
+    package_data={'cs2rotamer': ['resources/*']},
+    ext_modules=cythonize('pyroshiftGA/*.pyx')
+)
