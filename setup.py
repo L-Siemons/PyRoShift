87f827225ba04f82a5b41bb02230a9e46f9e75c5
Lucas Siemons <lucasSiemons@MacBook-Pro-4.local>
2018-12-26 13:12:57 +0000

    26.12.18 corrected setup.py

--- setup.py
+++ setup.py
@@ -1,34 +1,30 @@
-from distutils.core import setup
 from setuptools import setup, find_packages
 
 descrip='''
-
      ====// cs2rotamer \\\\====
 
-This package looks to determine protien 
-side-chain rotamer populations from chemical 
-shifts. 
+This package looks to determine protien
+side-chain rotamer populations from chemical
+shifts.
 
-Currently only isoleucine is implimented here.
-This package is principally maintained by 
-Lucas Siemons 
+Currently only isoleucine is implemented here.
+This package is principally maintained by
+Lucas Siemons
 
 Author:
-Lucas Siemons 
+Lucas Siemons
 
 '''
 
 setup(
-    name='cs2rotamer',
+    name='pyroshift',
     version='1.0',
     author='L. Siemons',
     author_email='zcbtla0@ucl.ac.uk',
     packages=find_packages(),
-    #scripts=[''],
-    #url='http://pypi.python.org/pypi/TowelStuff/',
     license='LICENSE.txt',
     description=descrip,
     long_description=open('README.md').read(),
-    #install_requires=['os','sys','random','numpy','time',],
-    package_data={'cs2rotamer': ['resources/*']}
+    install_requires=['os','sys','random','numpy','time','scipy'],
+    package_data={'pyroshift': ['resources/*']}
 )
