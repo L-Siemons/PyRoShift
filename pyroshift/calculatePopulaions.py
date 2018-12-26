2f03e03c5cf15edfae2cb45efc60c66f0eafdc4e
Lucas Siemons <lucasSiemons@MacBook-Pro-4.local>
2018-12-26 13:20:25 +0000

    26.12.18 pylint and added docstrings

--- pyroshift/calculatePopulaions.py
+++ pyroshift/calculatePopulaions.py
@@ -5,35 +5,50 @@ from chemical shifts
 
 import numpy as np
 from scipy.optimize import least_squares
+from .fileIO import Input, Output
 
-import pyroshift
-from .fileIO import Input
-
-class isoleucine(Input):
+class Isoleucine(Input, Output):
 
     '''
     This is the class for the user and calculates the rotamer distributions
     using a linear approach.
 
+    Note that in order for this method to be effective all 5 chemical shifts must be
+    provided and that should be referenced to DSS (not the TMS).
+
+    This class initializes two classes to deal with the inputs and outputs for this method.
+    The methods and attributes from these are documented in their respective class
+    in pyroshift/FileIo.py
+
+
+
     Attributes:
     ===========
+    atoms : list
+        - contains all the names for the side-chain
 
-    Methods:
-    ========
-
+    populations : nested dictionary
+    - key is the residue
+    - second key is the population
+    - entry is a tuple containing floats :
+        - (population, error)
     '''
 
-    def __init__(self, file,shift_matrix='default', ref_opt_file='default'):
+    def __init__(self, file, shift_matrix='default', ref_opt_file='default'):
 
         '''
-        This collects all the required inputs.
+        This collects all the required inputs and initializes the classes
+        Input and Output.
+
+        The attributes these two classes create are documents under their respective class
         '''
 
         #read everything in!
-        Input.__init__(self,file, shift_matrix=shift_matrix, ref_opt_file=ref_opt_file)
+        Input.__init__(self, file, shift_matrix=shift_matrix, ref_opt_file=ref_opt_file)
+        Output.__init__(self,)
         self.atoms = ['ca', 'cb', 'cg1', 'cg2', 'cd1']
 
-    def eq(self,state_matrix, pops_vector):
+    def eq(self, state_matrix, pops_vector):
         '''
         The equation for the linear system:
 
@@ -54,7 +69,7 @@ class isoleucine(Input):
         '''
         return np.dot(state_matrix.T, pops_vector)
 
-    def resid(self, pops_vector,cs_vector, matrix,):
+    def resid(self, pops_vector, cs_vector, matrix):
         '''
         Calculate the residual for the chi squared. Here
         we also add a restraint so the populations sum to 1.
@@ -73,8 +88,8 @@ class isoleucine(Input):
         '''
 
         cs_model = self.eq(matrix, pops_vector)
-        resdidual =  cs_model - cs_vector
-        restraint = 1e4*(np.sum(pops_vector) - 1. )
+        resdidual = cs_model - cs_vector
+        restraint = 1e4*(np.sum(pops_vector) - 1.)
         total = np.append(resdidual, restraint)
         return total
 
@@ -94,13 +109,13 @@ class isoleucine(Input):
         result.x : np.array
             populations from the fit.s
         '''
-        lb=np.zeros(5)
-        ub=lb+1
+        lb = np.zeros(5)
+        ub = lb+1
 
-        start_pops = np.array([0.2,0.2,0.2,0.2,0.2])
+        start_pops = np.array([0.2, 0.2, 0.2, 0.2, 0.2])
         args = [cs_vector, state_matrix]
 
-        result = least_squares( self.resid, start_pops, bounds=(lb,ub), args=args )
+        result = least_squares(self.resid, start_pops, bounds=(lb, ub), args=args)
         return result.x
 
     def calc_pop_for_all_opts(self, state_matrix, cs_vector, sse):
@@ -132,7 +147,7 @@ class isoleucine(Input):
         pops_list = np.array(pops_list).T
         pops_dict = {}
 
-        for i,j in zip(pops_list, self.state_order[sse]):
+        for i, j in zip(pops_list, self.state_order[sse]):
             val = np.mean(i)
             std = np.std(i)
             pops_dict[j] = (val, std)
@@ -141,7 +156,25 @@ class isoleucine(Input):
 
     def calc_pops_for_all_residues(self, ):
         '''
-        calculate populations for all residues
+        Calculate populations for all residues that are specified in the the input file.
+        We do this by solving the linear system
+
+        C_states * P = C_exp
+
+        Under the constraint sum(P_i) = 1. where;
+        - C_states is a matrix containing the chemical shifts for each state
+        - P is a vector containing the populations
+        - C_exp is a vector with the experimental populations
+
+        This method defined self.populations
+
+        Returns:
+        =======
+        pops : nested dictionary
+            - key is the residue
+            - second key is the population
+            - entry is a tuple containing floats :
+                - (population, error)
         '''
 
         print 'calculating the populations ...'
