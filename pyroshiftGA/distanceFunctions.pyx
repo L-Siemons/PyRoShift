b0b8984d0c68697011697415dcd84e3fc28d03b4
Lucas Siemons <lucasSiemons@MacBook-Pro-4.local>
2018-12-27 12:01:42 +0000

    27.12.18 added c types

--- pyroshiftGA/distanceFunctions.pyx
+++ pyroshiftGA/distanceFunctions.pyx
@@ -1,10 +1,10 @@
 '''
-This library is for functions that calculate various different 
-distance measures. Here we have also included functions 
-that manipulate these distances. 
+This library is for functions that calculate various different
+distance measures. Here we have also included functions
+that manipulate these distances.
 '''
 
-import numpy as np
+import math
 
 ######################################################################
 # Basic distance functions
@@ -12,26 +12,32 @@ import numpy as np
 
 def Euclid_dis(observed,computed,params):
     '''
-    This function calculates the euclidean distance between the 
+    This function calculates the euclidean distance between the
     computed chemical shifts and the observed chemical shifts
 
     Parameters
     =========
     observed : list
-    computed : list 
-    params : Parameters class 
-    
+    computed : list
+    params : Parameters class
+
     Returns
     =======
-    final_distance : float 
+    final_distance : float
         The euclidian distance
     '''
-    
-    distance_sum = 0
+
+    cdef float distance_sum = 0.
+    cdef int carbon
+    cdef float a
+    cdef float b
+    cdef float c
+    cdef float final_distance
+
     for carbon in range(len(params.Side_chain_carbons)):
 
         if observed[params.Side_chain_carbons[carbon]] != "none":
-                
+
             #this bit is just the sum for the distance over many lines
             a = float(computed[carbon])
             b =  float(observed[params.Side_chain_carbons[carbon]])
@@ -39,7 +45,7 @@ def Euclid_dis(observed,computed,params):
             d = c**2
             distance_sum = distance_sum + d
 
-    final_distance = np.sqrt(distance_sum)
+    final_distance = math.sqrt(distance_sum)
     return final_distance
 
 ######################################################################
@@ -48,73 +54,81 @@ def Euclid_dis(observed,computed,params):
 
 def Distance(computed, observed,params):
     '''
-    The function calculates the selected distance measure between 
+    The function calculates the selected distance measure between
     each individual's chemical shift and the the observed chemical shift.
 
     Parameters
     ==========
     computed : dictionary
-        key - individual id 
+        key - individual id
         entry - list of chemical shifts
 
     observed : dictionary
-        key - carbon id 
+        key - carbon id
         entry - observed chemical shift
-    params : Parameters class 
+    params : Parameters class
 
 
     Returns
     =======
-    distance : dictionary 
-        key - individual id 
+    distance : dictionary
+        key - individual id
         entry - distance between the individuals chemical shifts
                 and the measured chemical shifts
     '''
-    
+
+    cdef int individual
     distance = {}
-    for individual in computed:         
+
+    for individual in computed:
         distance[individual] = params.disMeasure(observed,computed[individual],params)
     return distance
 
 def reciprocal(dict):
-    
+
     '''
     calculates the reciprocal of the distance for each individual.
-    
+
     Parameters
     ==========
-    dict : dictionary 
+    dict : dictionary
 
     Returns
     =======
     recip : dictionary
     '''
 
+    cdef float a
+    cdef int key
+
     recip = {}
-    for key in dict: 
-        a = np.divide(1,(dict[key]))
+    for key in dict:
+        a = 1./(dict[key])
         recip[key] = a
-    
+
     return recip
 
-    
+
 def get_smallest_distance(distances):
     '''
     Gets the smallest value in a dictionary
 
     Parameters
-    ========== 
+    ==========
     distances : dict
 
     Returns
-    =======   
+    =======
     s_indiv : float
-        individual id 
-    smallest : float 
+        individual id
+    smallest : float
         the value of the distance
     '''
-    
+
+    cdef float smallest
+    cdef int i
     smallest = 1000.
+
     for i in distances:
         if distances[i] <= smallest:
             s_indiv = i
@@ -123,43 +137,43 @@ def get_smallest_distance(distances):
     return s_indiv, smallest
 
 #===========================================================================
-#Old functions 
+#Old functions
 #===========================================================================
 #
 # def compare_populations(generated_rotamers):
-#     '''This also just does a euclidean distance between the generated population and a 
+#     '''This also just does a euclidean distance between the generated population and a
 #     solution but since there is a slightly different functionality compared to Euclidean_Distance()
 #     i decided to be lazy ...'''
-    
-#     distance = 0 
-    
+
+#     distance = 0
+
 #     #try statement is there for the first simulation which would have no previous solutions to compare to
 #     try:
-        
+
 #         solutions = open('Solutions.txt', 'r')
-#         #each line in S_pops is a set of rotamers which is a solution 
+#         #each line in S_pops is a set of rotamers which is a solution
 #         S_pops = solutions.readlines()
 #         smallest_distance = 100
 
 #         for list in S_pops:
 #             distance_sum = 0
 #             list = list.split()
-            
+
 #             for item in range(len(list)):
 
 #                 a = float(generated_rotamers[item])
 #                 b = float(list[item])
 #                 c = (a - b)
 #                 d = c**2
-#                 distance_sum = distance_sum+ d        
-            
+#                 distance_sum = distance_sum+ d
+
 #             if distance_sum <= smallest_distance:
 #                 smallest_distance = distance_sum
-        
+
 #         distance = smallest_distance
-        
-#     except IOError: 
+
+#     except IOError:
 #         distance = 1
 
 #     return distance
-#     
+#
