3fa96ac1e8599a5c72229a43c10fcedff23fa545
L_siemons <lucas.siemons@gmail.com>
2019-02-17 13:38:43 +0000

    17.02.19 added matplotlib to requirements

--- setup.py
+++ setup.py
@@ -25,6 +25,6 @@ setup(
     license='LICENSE.txt',
     description=descrip,
     long_description=open('README.md').read(),
-    install_requires=['numpy','scipy'],
+    install_requires=['numpy','scipy', 'matplotlib'],
     package_data={'pyroshift': ['resources/*']}
 )
